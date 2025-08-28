271c57d385e2b07737fbccdde0e4f10b0383d5d8
alaaibr25 <94000702+alaaibr25@users.noreply.github.com>
2025-08-28 23:11:08 +0300

    Update and rename main2.py to main.py
    Add log Flask form.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap5
 #◻🔘◻**********************◻🔘◻**********************◻🔘◻#
 app = Flask(__name__)
+bstrap = Bootstrap5(app)
+
+
+#◻🔘◻**********************◻🔘◻**********************◻🔘◻#
+
+class LogForm(FlaskForm):
+    email = EmailField('', validators=[DataRequired()])
+    pw_form = PasswordField('', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 
 #◻🔘◻**********************◻🔘◻**********************◻🔘◻#
-
-
-
-
-
-#◻🔘◻**********************◻🔘◻**********************◻🔘◻#
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def log_page():
-    return render_template('login.html')
+    form = LogForm()
+    return render_template('login.html', form=form)
 
 @app.route('/main')
 def main_page():
@@ -24,6 +32,7 @@ def main_page():
 
 app.run(debug=True)
 #◻🔘◻**********************◻🔘◻**********************◻🔘◻#
+
 
 
 
